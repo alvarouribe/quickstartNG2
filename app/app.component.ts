7d5e2b156a85585211869dadff37c90e43d52c58
Alvaro Uribe <alvaro.uribe@spmassets.com>
2016-04-12 20:34:59 +1200

    Added some comments and I have modified the appComponents to display more text in my first component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,5 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
+    					+ '<h2>Alvaro Uribe</h2>'
+    					+ '<h3>April 2016</h3>'
 })
 export class AppComponent { }
