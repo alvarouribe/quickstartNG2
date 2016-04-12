3bbde49a1a4cf050de57abafe3df18672f1f73d3
Alvaro Uribe <alvaro.uribe@spmassets.com>
2016-04-12 21:27:02 +1200

    Set first chapter for our heroes app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,73 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+  id: number;
+  name: string;
+  description: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+
+    // let’s take advantage of the template strings feature in ES2015 and TypeScript to maintain our sanity.
+    // template: '<h1> {{ title }} </h1>'
+    // 				 	+'<h2> {{hero.name}} details!</h2>'
+    // 				 	+'<p>{{hero.description}}</p>'
+    // 				 	+'<div><label>id: </label>{{hero.id}}</div>'
+		// 		 			+'<div> <label>name: </label>'
+		// 		 		  +'<input [(ngModel)]="hero.name" placeholder="name">'
+		// 		 	    +'</div>'
+
+    template:`<h1> {{ title }} </h1>
+  			  	 	<h2> {{hero.name}} details! </h2>
+				 	 		<p> {{hero.description}} </p>
+  				 	 	<div>
+  				 	 		<label>id: </label>{{hero.id}}
+  				 	 	</div>
+		 			   	
+		 			   	<div>
+						    <label>name: </label>
+						    <input [(ngModel)]="hero.name" placeholder="name">
+						  </div>`
+
+
+    // {{}} The double curly braces tell our app to read the title and hero properties from the component 
+    // and render them. This is the "interpolation" form of one-way data binding. 
+
 })
-export class AppComponent { }
+
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+	  id: 1,
+	  name: 'Alfie',
+		description: 'The power of self inflict selfies'
+	};
+
+}
+
+
+// import {Component} from 'angular2/core';
+// export class Hero {
+//   id: number;
+//   name: string;
+// }
+// @Component({
+//   selector: 'my-app',
+//   template:`
+//     <h1>{{title}}</h1>
+//     <h2>{{hero.name}} details!</h2>
+//     <div><label>id: </label>{{hero.id}}</div>
+//     <div>
+//       <label>name: </label>
+//       <input [(ngModel)]="hero.name" placeholder="name">
+//     </div>
+//     `
+// })
+// export class AppComponent {
+//   title = 'Tour of Heroes';
+//   hero: Hero = {
+//     id: 1,
+//     name: 'Windstorm'
+//   };
+// }
